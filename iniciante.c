77b0ba9d635535bc4a9611b04bb684796c21c797
Jean Castro <jean.castro.42@gmail.com>
2026-02-03 00:30:57 +0000

    PREPARAÇÃO DE CARTAS

---
+++ iniciante.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+ 
+int main(){
+//----CARTA 1
+char estado1;
+char codigo1 [10];
+char cidade1 [10];       //string
+float populaçao1;
+float area1;
+float pib1;
+int pontos1;
+
+ //----carta 2
+
+char estado2;
+char codigo2 [10];  //STRING
+char cidade2 [10];  //string
+float populaçao2;
+float area2;
+float pib2;
+int pontos2; 
+
+//LEITURA CARTA 1
+printf("CARTA 1");
+
+printf("ESTADO: ");
+
+printf("CODIGO: ");
+
+printf("CIDADE: ");
+
+printf("POPULAÇÃO");
+
+printf("ÁREA: ");
+
+printf("PIB: ");
+
+printf("PONTOS TURISTICOS: ");
+    
+return 0;
+}
